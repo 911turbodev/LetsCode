749e0f52b7240dcf8c3bb94e823b374e70607c44
911turbodev <supragoodbye@gmail.com>
2025-04-04 00:45:01 -0300

    desafios

---
+++ Desafios/desafio16.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    // Ejercicio [8]
+    // El programa debe solicitar tres números y mostrar cuál es el mayor.
+    // Si hay dos números iguales al mayor, indicarlo.
+    int a, b, c;
+    printf("Ingrese 3 numeros:\n");
+    scanf("%d %d %d", &a, &b, &c);
+
+    // Determinar el mayor número
+    if (a >= b && a >= c)
+    {
+        if (a == b && a == c)
+        {
+            printf("[%d, %d y %d] son iguales\n", a, b, c);
+        }
+        else if (a == b)
+        {
+            printf("[%d y %d] son mayores e iguales\n", a, b);
+        }
+        else if (a == c)
+        {
+            printf("[%d y %d] son mayores e iguales\n", a, c);
+        }
+        else
+        {
+            printf("[%d] es el mayor\n", a);
+        }
+    }
+    else if (b >= a && b >= c)
+    {
+        if (b == c)
+        {
+            printf("[%d y %d] son mayores e iguales\n", b, c);
+        }
+        else
+        {
+            printf("[%d] es el mayor\n", b);
+        }
+    }
+    else
+    {
+        printf("[%d] es el mayor\n", c);
+    }
+
+    return 0;
+}
