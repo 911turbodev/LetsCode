9a93591aa0060aaebb19f087123ca4be340b7f34
911turbodev <supragoodbye@gmail.com>
2025-04-04 03:39:04 -0300

    Funcion posicionar cursor en la consola y getch()

--- Desafios/desafio18.c
+++ Desafios/desafio18.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <windows.h> // Para usar gotoxy y otras funciones de Windows
+
+// Función para posicionar el cursor en la consola
+void gotoxy(int x, int y)
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
 int main()
 {
     // Ejercicio [10]
@@ -8,75 +19,120 @@ int main()
     // Euros a Dólares
     int opcion;
     float ars, eur, usd;
+
     do
     {
-        printf("**************************\n");
-        printf("* [Conversion de moneda] *\n");
-        printf("**************************\n");
-        printf("*   1. Dolares a Euros   *\n");
-        printf("*   2. Dolares a Pesos   *\n");
-        printf("*   3. Euros a Dolares   *\n");
-        printf("*   0. Para finalizar    *\n");
-        printf("**************************\n");
+        system("cls"); // Limpia la pantalla
+        gotoxy(40, 3);
+        printf("=====================================");
+        gotoxy(40, 4);
+        printf("=          MENU DE CONVERSION       =");
+        gotoxy(40, 5);
+        printf("=====================================");
+        gotoxy(40, 6);
+        printf("=   1. Dolares a Euros              =");
+        gotoxy(40, 7);
+        printf("=   2. Dolares a Pesos              =");
+        gotoxy(40, 8);
+        printf("=   3. Euros a Dolares              =");
+        gotoxy(40, 9);
+        printf("=   0. Para finalizar               =");
+        gotoxy(40, 10);
+        printf("=====================================");
+        gotoxy(40, 11);
         printf("Ingrese una opcion: ");
         scanf("%d", &opcion);
+
         switch (opcion)
         {
         case 1:
-            printf("\nConversor USD a EUR\n");
+            system("cls");
+            gotoxy(40, 3);
+            printf("Conversor USD a EUR");
             do
             {
-                printf("Ingrese cantidad de USD que desea convertir a Euros:\n");
-                scanf("%f.2", &usd);
+                gotoxy(40, 5);
+                printf("Ingrese cantidad de USD que desea convertir a Euros: ");
+                scanf("%f", &usd);
                 if (usd > 0)
                 {
-                    printf("[%.2f USD a EUR son: %.2f] \n\n", usd, 0.90 * usd);
+                    gotoxy(40, 7);
+                    printf("[%.2f USD a EUR son: %.2f]\n", usd, 0.90 * usd);
                 }
                 else
                 {
-                    printf("Valor ingresado invalido.\n");
+                    gotoxy(40, 8);
+                    printf("Valor ingresado invalido. Intente nuevamente.\n");
                 }
-                break;
-            } while (usd > 0);
+            } while (usd <= 0);
+            gotoxy(40, 10);
+            printf("Presione cualquier tecla para volver al menu...");
+            getch(); // Espera una tecla para continuar
             break;
+
         case 2:
-            printf("\nConversor USD a Ars\n");
+            system("cls");
+            gotoxy(40, 3);
+            printf("Conversor USD a ARS");
             do
             {
-                printf("Ingrese cantidad de USD que desea convertir a Ars:\n");
-                scanf("%f.2", &usd);
+                gotoxy(40, 5);
+                printf("Ingrese cantidad de USD que desea convertir a ARS: ");
+                scanf("%f", &usd);
                 if (usd > 0)
                 {
-                    printf("[%.2f USD a Ars son: %.2f] \n\n", usd, 1310 * usd);
+                    gotoxy(40, 7);
+                    printf("[%.2f USD a ARS son: %.2f]\n", usd, 1310 * usd);
                 }
                 else
                 {
-                    printf("Valor ingresado invalido.\n");
+                    gotoxy(40, 8);
+                    printf("Valor ingresado invalido. Intente nuevamente.\n");
                 }
-                break;
-            } while (usd > 0);
+            } while (usd <= 0);
+            gotoxy(40, 10);
+            printf("Presione cualquier tecla para volver al menu...");
+            getch(); // Espera una tecla para continuar
             break;
+
         case 3:
-            printf("\nConversor Eur a USD\n");
+            system("cls");
+            gotoxy(40, 3);
+            printf("Conversor EUR a USD");
             do
             {
-                printf("Ingrese cantidad de Eur que desea convertir a USD:\n");
-                scanf("%f.2", &eur);
+                gotoxy(40, 5);
+                printf("Ingrese cantidad de EUR que desea convertir a USD: ");
+                scanf("%f", &eur);
                 if (eur > 0)
                 {
-                    printf("[%.2f Eur a USD son: %.2f] \n\n", eur, 1.11 * eur);
+                    gotoxy(40, 7);
+                    printf("[%.2f EUR a USD son: %.2f]\n", eur, 1.11 * eur);
                 }
                 else
                 {
-                    printf("Valor ingresado invalido.\n");
+                    gotoxy(40, 8);
+                    printf("Valor ingresado invalido. Intente nuevamente.\n");
                 }
-                break;
-            } while (usd > 0);
+            } while (eur <= 0);
+            gotoxy(40, 10);
+            printf("Presione cualquier tecla para volver al menu...");
+            getch(); // Espera una tecla para continuar
+            break;
+
+        case 0:
+            system("cls");
+            gotoxy(40, 5);
+            printf("Finalizando programa...");
+            break;
+
+        default:
+            gotoxy(40, 13);
+            printf("Opcion invalida. Intente nuevamente.\n");
+            Sleep(2000); // Pausa de 2 segundos antes de volver al menú
+            break;
         }
     } while (opcion != 0);
-    if (opcion == 0)
-    {
-        printf("Finalizando programa...");
-    }
+
     return 0;
 }
