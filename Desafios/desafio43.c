11c2061cda52e36ce0d23074bacfd2b07aa924b4
911turbodev <supragoodbye@gmail.com>
2025-04-15 18:43:02 -0300

    desafios

--- Desafios/desafio43.c
+++ Desafios/desafio43.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 int main()
 {
-
+    int i, j, num, esPrimo, contadorPrimos = 0;
+    for (i = 1; i <= 10; i++)
+    {
+        printf("POS [%i] Ingrese un numero ", i);
+        scanf("%d", &num);
+        if (num <= 1)
+        {
+            esPrimo = 0;
+        }
+        else
+        {
+            esPrimo = 1;
+            for (j = 2; j * j <= num; j++)
+            {
+                if (num % j == 0)
+                {
+                    esPrimo = 0;
+                    break;
+                }
+            }
+            if (esPrimo == 1)
+            {
+                contadorPrimos++;
+            }
+        }
+    }
+    printf("Contador de primos es: %d", contadorPrimos);
     return 0;
 }
